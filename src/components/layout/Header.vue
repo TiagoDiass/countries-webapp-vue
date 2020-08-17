5a651bdf774c0974eaa5054039d18f95972df76b
TiagoDiass <tiago.costadiasss@gmail.com>
2020-08-17 17:32:24 -0300

    improving components and adding icons

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+    <h1>Where in the world?</h1>
+
+    <button v-if="darkTheme" @click="darkTheme = false">
+      <i class="fas fa-sun"></i>
+      Light Theme
+    </button>
+    <button v-else @click="darkTheme = true">
+      <i class="fas fa-moon"></i>
+      Dark Theme
+    </button>
+  </header>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    darkTheme: false,
+  }),
+};
+</script>
