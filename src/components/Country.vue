3d7f11b8020ac44c2c6725faf7529583b3d2f446
TiagoDiass <tiago.costadiasss@gmail.com>
2020-08-18 09:37:16 -0300

    homepage done

--- src/components/Country.vue
+++ src/components/Country.vue
@@ -1,7 +1,24 @@
 <template>
   <div class="country">
-    {{ country.name }}
-    <img style="width: 150px;" :src="country.flag" />
+    <div class="country-flag" :style="`background-image: url(${country.flag})`"></div>
+    <div class="country-data">
+      <h3>{{ country.name }}</h3>
+
+      <!-- Population -->
+      <div class="data-row">
+        <span class="label">Population:</span> <span class="data">{{ country.population.toLocaleString() }}</span>
+      </div>
+
+      <!-- Region -->
+      <div class="data-row">
+        <span class="label">Region:</span> <span class="data">{{ country.region }}</span>
+      </div>
+
+      <!-- Capital -->
+      <div class="data-row">
+        <span class="label">Capital:</span> <span class="data">{{ country.capital }}</span>
+      </div>
+    </div>
   </div>
 </template>
 
