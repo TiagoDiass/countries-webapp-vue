5fb1189851a7287147d328afad2f0acba8e0a710
TiagoDiass <tiago.costadiasss@gmail.com>
2020-08-17 22:11:50 -0300

    search bar and header done

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,49 @@
 <template>
-  <div class="home">
-    <h1>manoooo</h1>
+  <div class="home view">
+    <div class="search-row">
+      <div class="search-bar">
+        <i class="fas fa-search"></i>
+        <input type="text" placeholder="Search for a country..." />
+      </div>
+      <div class="region-filter">
+        <select name="region" id="region">
+          <option :value="null" selected>Filter by Region</option>
+          <option v-for="region in regions" :key="region.value" :value="region.value">{{ region.placeholder }}</option>
+        </select>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Home',
+
+  computed: {
+    regions() {
+      return [
+        {
+          placeholder: 'Africa',
+          value: 'Africa',
+        },
+        {
+          placeholder: 'America',
+          value: 'America',
+        },
+        {
+          placeholder: 'Asia',
+          value: 'Asia',
+        },
+        {
+          placeholder: 'Europe',
+          value: 'Europe',
+        },
+        {
+          placeholder: 'Oceania',
+          value: 'Oceania',
+        },
+      ];
+    },
+  },
 };
 </script>
